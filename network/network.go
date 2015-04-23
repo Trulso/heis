057c50b53be806f1103ea2f6d8338d97ee13ee09
Trulso <Trulso@msn.com>
2015-04-23 15:05:20 +0200

    Updated Network

--- network/network.go
+++ network/network.go
@@ -7,59 +7,63 @@ import (
 )
 
 
-type Dummy struct {
+type ElevatorInfo struct { // not done. 
        s int
-       b int
+       b int 
 }
 
 
 
-func UDPListen(data chan Dummy ,port int) {
-
+func UDPListen(rx chan ElevatorInfo ,port int){
+	var temp_struct info
 	local,error:= net.ResolveUDPAddr("udp",fmt.Sprintf(":%d", port)) 
 	if error !=nil{
-		fmt.Printf("Error Resolving UDP")
-
-
-		
+		fmt.Println("error:", error)		
 	}
 
 
 	socket,error:=net.ListenUDP("udp",local)
 	if error !=nil{
-		fmt.Printf("Error Listening to local adress")
+		ffmt.Println("error:", error)
 		
 	}
+	socket.SetReadDeadline(time.Now().Add(10*time.Second)) //ingen aktivitet på net i løpet av 10s, noe er feil ?=
+
 	for{
-		dummy_st  := Dummy{}
 		buffer := make([]byte,1024)
-		_,_,error := socket.ReadFromUDP(buffer) 
+		n,_,error := socket.ReadFromUDP(buffer) 
 		if error !=nil{
-			fmt.Printf("Error Reading from UDP")
-			
+			fmt.Println("error:", error)
+			//Mulig status chan
 		}
-		json.Unmarshal(buffer, &dummy_st)
-		data <- dummy_st 
+
+		error = json.Unmarshal(buffer[:n], &temp_struct)
+		if error != nil {
+			fmt.Println("error:", error)
+		}
+		rx <- temp_struct
+		//Mulig status chan
 	}
 
 }
 
-func UDPSend(data chan Dummy ,port int)  {
+func UDPSend(tx chan ElevatorInfo ,port int)  {
 
 	casting,error:= net.ResolveUDPAddr("udp",fmt.Sprintf("255.255.255.255:%d", port)) 
 	if error !=nil{
-		fmt.Printf("Error Resolving UDP")	
+		fmt.Println("error:", error)	
 	}
 	socket,error:=net.DialUDP("udp",nil,casting)
 	if error !=nil{
-		fmt.Printf("Error Dialing to UDP adress")
-		
+		fmt.Println("error:", error)	
 	}
-
-	buffer := <- data
-	un_buffer,_ := json.Marshal(buffer)
-	_,error = socket.Write(un_buffer)
+	socket.SetWriteDeadline(time.Now().Add(10*time.Second))
+	for{
+		buffer := <- tx
+		un_buffer,_ := json.Marshal(buffer)
+		_,error = socket.Write(un_buffer)
 		if error !=nil{
-		fmt.Printf("Error writing to UDP adress")
+			fmt.Println("error:", error)
+		}
 	}
 }
